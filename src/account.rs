ec897a1a4a80f7f02bf64e503df0ba1cc296cda2
Chris Prohoda <cgprohoda@gmail.com>
2017-06-18 20:30:12 -0500

    Initial commit

---
+++ src/account.rs
@@ -0,0 +1,52 @@
+use std:Collections::HashMap;
+
+struct User {
+	username: str;
+	displayName: str;
+	balance: f32;
+	history: Vec<str>;
+	skills: HashMap<&str, u32>;
+	verified: bool;
+	permissions: PermissionLevel;
+}
+
+enum PermissionLevel {
+	limited,
+	full,
+	admin,
+}
+
+impl User {
+	fn New() -> user {
+		// read from DB
+	}
+
+	pub fn ChangeDisplayName(&mut self, newName: str) {
+		self.displayName = newName;
+	}
+
+	pub fn ChangePassword(&mut self, oldPassword: str, newPassword: str) {
+		// verify password here
+		self.password = newPassword;
+	}
+
+	pub fn ViewHistory(self) {
+		self.history
+	}
+
+	pub fn ViewSkills(self) {
+		self.skills
+	}
+
+	pub fn ReturnRelevantSkills(self, relevantSkills: Vec<str>) {
+
+	}
+
+	pub fn PostContract(self, )
+}
+
+pub fn login(username, password) -> user {
+	// verify password here
+	User.new(username)
+
+}
---
+++ src/contract.rs
@@ -0,0 +1,3 @@
+struct contract {
+	date
+}
