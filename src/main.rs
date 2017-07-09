40ab0ef1ab0f75afffc99b4f4b377a3451e708ee
Chris Prohoda <cgprohoda@gmail.com>
2017-07-08 23:57:41 -0500

    Initial webapp framework

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod account;
 mod contract;
 mod webapp;
+
 extern crate time;
+extern crate rocket;
+
+#[get("/contract")]
+fn contract() -> &'static str {
+	"awwww yeeee"
+}
 
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/contract", routes![contract]).launch();
 }
